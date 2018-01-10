cefef3348d29ea0ab8bbee59f97ae68496646d14
Amrit Bhattacharjee <ab181094@gmail.com>
2018-01-10 09:45:26 +0600

    Added load image from url

--- app/src/main/java/com/example/amrit/demo/MainActivity.java
+++ app/src/main/java/com/example/amrit/demo/MainActivity.java
@@ -2,14 +2,13 @@ package com.example.amrit.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.amrit.demo.loadImage.LoadImageActivity;
 import com.example.amrit.demo.notificationBadge.NotificationBadgeActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void notificationBadge(View view) {
         Intent intent = new Intent(MainActivity.this, NotificationBadgeActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadImage(View view) {
+        Intent intent = new Intent(MainActivity.this, LoadImageActivity.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/amrit/demo/loadImage/LoadImageActivity.java
@@ -0,0 +1,50 @@
+package com.example.amrit.demo.loadImage;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.amrit.demo.R;
+import com.squareup.picasso.Picasso;
+
+public class LoadImageActivity extends AppCompatActivity {
+    String image_url = "http://www.hdwallpapersfreedownload.com/uploads/large/super-heroes/" +
+            "hd-free-download-hero-of-superman.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_image);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView imageView = findViewById(R.id.loadImage_imageView);
+        Picasso.with(this).load(image_url).placeholder(R.mipmap.ic_launcher)
+                .error(R.mipmap.ic_launcher)
+                .into(imageView, new com.squareup.picasso.Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+    }
+}
